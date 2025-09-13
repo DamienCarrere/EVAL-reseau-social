d480ec68261970ad76c5b92a38e2884e121306dc
Brian Farnier <brian.pro1993@gmail.com>
2025-09-13 19:18:29 +0200

    Refactor MyProfile component: remove MyProfile.css, integrate ProfileLayout for user display, and add styling to ProfileLayout.css.

--- pages/profile/MyProfile.jsx
+++ pages/profile/MyProfile.jsx
@@ -1,6 +1,6 @@
 import useUserData from "../../API/useUserData";
 import PostSelected from "../../components/Post/PostSelected";
-import "./MyProfile.css";
+import ProfileLayout from "../../components/Layout/ProfileLayout";
 
 function MyProfile() {
 	const users = useUserData();
@@ -9,27 +9,12 @@ function MyProfile() {
 	if (!user) return <p>Chargement...</p>;
 
 	return (
-		<section>
-			<div className="profil-main">
-				<div className="img-profil">
-					<img src={user.image} alt={user.firstName}></img>
-				</div>
-				<div className="infoPlusSuvi">
-					<div className="info-profil">
-						<h2 className="h2-titre">
-							{user.firstName} {user.lastName}
-						</h2>
-						<p className="p-info">{user.age} ans</p>
-						<p className="p-info">{user.email}</p>
-					</div>
-					<div className="suivi">
-						<p className="p-suivi">Suivi : ???</p>
-					</div>
-				</div>
-			</div>
-
-			<PostSelected userSelect={user.id} />
-		</section>
+		<>
+			<ProfileLayout user={user}>
+				<p className="p-suivi">suivi :</p>
+			</ProfileLayout>
+			<PostSelected user={user.id} />
+		</>
 	);
 }
 
