92d664e70f8b3187313632437c945aa670a0ffb1
DamienCarrere <damien.carrere@protonmail.ch>
2025-09-13 14:50:54 +0200

    filter for users

--- pages/Accueil.jsx
+++ pages/Accueil.jsx
@@ -1,5 +1,19 @@
 import PostList from "../components/Post/PostList";
+import PostCard from "../components/Post/PostCard";
+import useUserData from "../API/useUserData";
+import usePostData from "../API/usePostData";
+import PostSelected from "../components/Post/PostSelected";
 
 export default function Accueil() {
-	return <PostList />;
+	const users = useUserData();
+	const posts = usePostData();
+	if (!posts.length || !users.length) {
+		return <p>Chargement...</p>;
+	}
+	return (
+		<div>
+			<PostList />
+			{/* <PostSelected userSelect={2} /> */}
+		</div>
+	);
 }
