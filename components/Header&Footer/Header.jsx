5f21017ecf8c1185417a71e374a8ff30954ba64d
Brian Farnier <brian.pro1993@gmail.com>
2025-09-14 22:43:28 +0200

    add logo

--- components/Header&Footer/Header.jsx
+++ components/Header&Footer/Header.jsx
@@ -1,5 +1,16 @@
 import "./Header&Footer.css";
+import logo from "../../logo/logo.png";
 
 export default function Header() {
-	return <header className="header"></header>;
+	return (
+		<header className="header">
+			<img
+				className="logo"
+				src={logo}
+				alt="Logo"
+				width="100px"
+				height="auto"
+			></img>
+		</header>
+	);
 }
