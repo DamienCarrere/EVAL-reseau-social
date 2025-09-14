604eaa4bf4e5913752c28913f7f50a04c872e948
DamienCarrere <damien.carrere@protonmail.ch>
2025-09-14 23:08:46 +0200

    zegzegzeg

--- components/Header&Footer/Header.jsx
+++ components/Header&Footer/Header.jsx
@@ -1,5 +1,9 @@
 import "./Header&Footer.css";
 
 export default function Header() {
-	return <header className="header"></header>;
+	return (
+		<header className="header">
+			<img src="https://media.discordapp.net/attachments/1416859591265747147/1416878439969063002/telecharge_1.png"></img>
+		</header>
+	);
 }
